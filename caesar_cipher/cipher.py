5d5b1d1555f1293ed0ec4c17720e66328d3445c6
ianfshirley <ianfshirley88@gmail.com>
2023-01-11 17:56:25 -0800

    all three functions printing expected values

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -1,5 +1,7 @@
 import nltk
 import ssl
+import re
+
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -18,35 +20,68 @@ from nltk.corpus import words, names
 word_list = words.words()
 name_list = names.words()
 
-print(word_list)
+combined_list = (word_list + name_list)
+reference_list = [word.lower() for word in combined_list]
+
+
+
 
 def encrypt(plaintext, shift):
   """
   encode plaintext using a shift key
   """
+
   ciphertext = ""
   for char in plaintext:
+    shift_num = shift % 26
     if char.isalpha():
-      shift_num = ord(char) + shift
-      if char.isupper():
-        shift_num = shift_num % ord('Z') + ord('A') - 1
-      else:
-        shift_num = shift_num % ord('Z') + ord('A') - 1
-      ciphertext += chr(shift_num)
+
+      char_ord = ord(char) + shift_num
+      
+      if char_ord < 97:
+        char_ord += 26
+      elif char_ord > 122:
+        char_ord -= 26
+
+      ciphertext += chr(char_ord)
+      # print(char_ord)
     else:
-      ciphertext += char
+      ciphertext += " "    
+  # print(ciphertext)
   return ciphertext
+  
+
+
 
 
 def decrypt(ciphertext, shift):
   """
   decrypt the cipher text using the shift key
   """
-  pass
+  # print(encrypt(ciphertext, (shift * -1)))
+  return encrypt(ciphertext, (shift * -1))
 
 
 def crack(ciphertext):
   """
   decode an encrypted message without the key. use brute force, shift one letter at a time from 1-25 until the message is decoded
   """
-  pass
+
+  cracked = ""
+
+  for shift in range(26):
+    decrypted = decrypt(ciphertext, shift).split()
+
+    for word in decrypted:
+      if word in reference_list:
+        cracked += word + " "
+
+
+  return cracked
+
+
+if __name__ == "__main__":
+  # print(encrypt('hello world', 5))
+  # print(decrypt('mjqqt btwqi', 5))
+  print(crack('mjqqt btwqi'))
+
