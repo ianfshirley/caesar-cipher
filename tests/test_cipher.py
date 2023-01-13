bd0885f447760b95d21424a7aa49493fcd495b7b
ianfshirley <ianfshirley88@gmail.com>
2023-01-12 22:24:20 -0800

    uncommented tests, refactored cipher.py

--- tests/test_cipher.py
+++ tests/test_cipher.py
@@ -2,49 +2,49 @@ from caesar_cipher.cipher import encrypt, decrypt, crack
 
 
 
-# def test_encrypt_shift_1():
-#     actual = encrypt("apple", 1)
-#     expected = "bqqmf"
+def test_encrypt_shift_1():
+    actual = encrypt("apple", 1)
+    expected = "bqqmf"
+    assert actual == expected
+
+
+def test_encrypt_shift_10():
+    actual = encrypt("apple", 10)
+    expected = "kzzvo"
+    assert actual == expected
+
+
+def test_encrypt_shift_20():
+    actual = encrypt("apple", 20)
+    expected = "ujjfy"
+    assert actual == expected
+
+
+def test_uppercase():
+    actual = encrypt("BANANA", 10)
+    expected = "LKXKXK"
+    assert actual == expected
+
+
+# def test_with_whitespace():
+#     actual = encrypt("apples and bananas", 1)
+#     expected = "bqqmft boe cbobobt"
 #     assert actual == expected
 
 
-# def test_encrypt_shift_10():
-#     actual = encrypt("apple", 10)
-#     expected = "kzzvo"
-#     assert actual == expected
+def test_with_non_alpha():
+    actual = encrypt("Gimme a 1!", 1)
+    expected = "Hjnnf b 1!"
+    assert actual == expected
 
 
-# def test_encrypt_shift_20():
-#     actual = encrypt("apple", 20)
-#     expected = "ujjfy"
-#     assert actual == expected
-
-
-# def test_uppercase():
-#     actual = encrypt("BANANA", 10)
-#     expected = "LKXKXK"
-#     assert actual == expected
-
-
-# # def test_with_whitespace():
-# #     actual = encrypt("apples and bananas", 1)
-# #     expected = "bqqmft boe cbobobt"
-# #     assert actual == expected
-
-
-# def test_with_non_alpha():
-#     actual = encrypt("Gimme a 1!", 1)
-#     expected = "Hjnnf b 1!"
-#     assert actual == expected
-
-
-# def test_round_trip():
-#     original = "Gimme a 1!"
-#     shift = 5
-#     encrypted = encrypt(original, shift)
-#     actual = decrypt(encrypted, shift)
-#     expected = original
-#     assert actual == expected
+def test_round_trip():
+    original = "Gimme a 1!"
+    shift = 5
+    encrypted = encrypt(original, shift)
+    actual = decrypt(encrypted, shift)
+    expected = original
+    assert actual == expected
 
 
 def test_crack_phrase():
